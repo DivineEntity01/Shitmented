bbe4ba334d43615a5d6d57956ee14407bc1c0213
DivineEntity01 <78000390+DivineEntity01@users.noreply.github.com>
2021-03-20 22:05:14 -0600

    Update NotPartOfShitmented.lua

--- NotPartOfShitmented.lua
+++ NotPartOfShitmented.lua
@@ -27,6 +27,18 @@ function refresh(plr)
 	end)
 end
 
+local GC = getconnections or get_signal_cons
+
+if GC then
+		for i,v in pairs(GC(game.Players.LocalPlayer.Idled)) do
+			if v["Disable"] then
+				v["Disable"](v)
+			elseif v["Disconnect"] then
+				v["Disconnect"](v)
+			end
+		end
+end
+
 while Enabled do
 if game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart", 0.02) then
 if not Enabled then
